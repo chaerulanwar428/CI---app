27fff983f4093e294409638c973562204c318f28
chaerul anwar <chaerulanwar428@gmail.com>
2022-12-17 21:27:46 +0700

    menambahkan function getAllMahasiswa

--- application/controllers/Mahasiswa.php
+++ application/controllers/Mahasiswa.php
@@ -1,6 +1,12 @@
 <?php
 
 class Mahasiswa extends CI_Controller{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('Mahasiswa_model');
+    }
+
     public function index()
     {
         $data['judul'] = 'Daftar Mahasiswa';
