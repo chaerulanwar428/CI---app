d177e0d41c75de06037784317a61133e94b13220
chaerul anwar <chaerulanwar428@gmail.com>
2022-12-18 05:21:19 +0700

    menambahkan file detail

---
+++ application/views/mahasiswa/detail.php
@@ -0,0 +1,18 @@
+<div class="contaner">
+    <div class="row mt-3">
+        <div class="col-md-6">
+
+        <div class="card">
+        <div class="card-header">
+            Detail Data Mahasiswa
+        </div>
+        <div class="card-body">
+            <h5 class="card-title"><?= $mahasiswa['nama']; ?></h5>
+            <p class="card-text"></p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+        </div>
+
+        </div>
+    </div>
+</div>
