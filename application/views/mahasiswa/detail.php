66cfcf5e82c1e7977c9b8708332f41d8399e78e0
chaerul anwar <chaerulanwar428@gmail.com>
2022-12-18 05:24:48 +0700

    menambahkan fitur detail

--- application/views/mahasiswa/detail.php
+++ application/views/mahasiswa/detail.php
@@ -1,4 +1,4 @@
-<div class="contaner">
+<div class="container">
     <div class="row mt-3">
         <div class="col-md-6">
 
@@ -8,8 +8,10 @@
         </div>
         <div class="card-body">
             <h5 class="card-title"><?= $mahasiswa['nama']; ?></h5>
-            <p class="card-text"></p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
+            <h6 class="card-subtitle mb-2 text-muted"><?= $mahasiswa['email']; ?></h6>
+            <p class="card-text"><?= $mahasiswa['nim']; ?></p>
+            <p class="card-text"><?= $mahasiswa['jurusan']; ?></p>
+            <a href="<?= base_url(); ?>mahasiswa" class="btn btn-primary">Kembali</a>
         </div>
         </div>
 
