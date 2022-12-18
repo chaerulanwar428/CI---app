ebe62fcb47f6e8219a06f5cc50f3ec91d5317e39
chaerul anwar <chaerulanwar428@gmail.com>
2022-12-18 20:24:37 +0700

    menambahkan model peoples

--- application/controllers/Peoples.php
+++ application/controllers/Peoples.php
@@ -6,7 +6,9 @@ class Peoples extends CI_Controller{
     public function index()
     {
         $data['judul'] = 'List of Peoples';
-       
+       $this->load->model('Peoples_model', 'peoples');
+
+       $data['peoples'] = $this->peoples->getAllPeoples();
        
         $this->load->view('templates/header', $data);
         $this->load->view('peoples/index', $data);
---
+++ application/views/peoples/index.php
@@ -0,0 +1,29 @@
+<div class="container">
+    <div class="row">
+        <div class="col-md-10">
+            <h3 class="mt-3">List of Poeples</h3>
+            <table class="table">
+                <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Action</th>
+                </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th>1</th>
+                        <td>Chaerul</td>
+                        <td>chaerulanwar428@gmail.com</td>
+                        <td>
+                            <a href="" class="badge badge-warning">detail</a>
+                            <a href="" class="badge badge-success">ubah</a>
+                            <a href="" class="badge badge-danger">hapus</a>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
